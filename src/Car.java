680c2479eca6b956c3996f8fb4a02a2421f5a0ae
Maximilian Eckert <maxieckert@web.de>
2015-10-11 17:53:50 +0200

    Aufgabe 4

--- src/Car.java
+++ src/Car.java
@@ -24,4 +24,18 @@ public class Car {
 	boolean hasAirCondition;
 	
 	boolean hasHeatedSeats;
+
+	public Car(String manufacturer, String serialNumber, CarBody body, Engine engine, Gear gear, Wheel wheels,
+			Variant variant, boolean hasAirCondition, boolean hasHeatedSeats) {
+		this.manufacturer = manufacturer;
+		this.serialNumber = serialNumber;
+		this.body = body;
+		this.engine = engine;
+		this.gear = gear;
+		this.wheels = wheels;
+		this.variant = variant;
+		this.hasAirCondition = hasAirCondition;
+		this.hasHeatedSeats = hasHeatedSeats;
+	}
+	
 }
