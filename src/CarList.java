5c7b8de49ecbd403db2d4cfa7a069bc74bbeedf8
Maximilian Eckert <maxieckert@web.de>
2015-10-16 16:11:03 +0200

    Aufgabe 22

--- src/CarList.java
+++ src/CarList.java
@@ -1,5 +1,33 @@
 
 public class CarList {
+	
+	private class CarListElement {
+
+		private CarListElement next;
+		
+		private Car content;
+		
+		public CarListElement(Car content, CarListElement next) {
+			this.content = content;
+			this.next = next;
+		}
+
+		public CarListElement getNext() {
+			return next;
+		}
+
+		public void setNext(CarListElement next) {
+			this.next = next;
+		}
+
+		public Car getContent() {
+			return content;
+		}
+
+		public void setContent(Car content) {
+			this.content = content;
+		}
+	}
 
 	private CarListElement first;
 
