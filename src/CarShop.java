a9efd85499f734c13cc93e4e62f391655ec633b9
Maximilian Eckert <maxieckert@web.de>
2015-11-30 17:02:30 +0100

    minor changes

--- src/CarShop.java
+++ src/CarShop.java
@@ -23,18 +23,9 @@ public class CarShop {
 		
 		System.out.println("Car 1: " + car1);
 		
-		double totalPrice = car1.getTotalPrice(19);
 		
-		System.out.println(totalPrice);
-		System.out.println("\n");
-		System.out.println(car1.getPowerClass());
-		
-		System.out.println(car2 == car3);
-		System.out.println(car2.equals(car3));
-		
-		testCarListAddFirst(new Car[] {car1, car2, car3});
-		testCarListAddLast(new Car[] {car1, car2, car3});
-		testCarListRemove(new Car[] {car1, car2, car3}, car2);
+		System.out.println("Car2 = Car3 ? : " + (car2 == car3));
+		System.out.println("Car2 = Car3 ? : " + car2.equals(car3));
 	}
 
 	Car getMostExpCar(Car[] cars) {
@@ -48,31 +39,4 @@ public class CarShop {
 		
 		return mostExpCar;
 	}
-	
-	static void testCarListAddFirst(Car[] carArray) {
-		CarList list = new CarList();
-		for (int i = 0; i < carArray.length; i++) {
-			list.addFirst(carArray[i]);
-		}
-	    System.out.println(list);
-	}
-	
-	static void testCarListAddLast(Car[] carArray) {
-		CarList list = new CarList();
-		for (int i = 0; i < carArray.length; i++) {
-			list.addLast(carArray[i]);
-		}
-	    System.out.println(list);
-	}
-	
-	
-	static void testCarListRemove(Car[] cars, Car car) {
-		CarList list = new CarList();
-		for (int i = 0; i < cars.length; i++) {
-			list.addFirst(cars[i]);
-		}
-	    list.remove(car);
-	    System.out.println(list);
-	}
-
 }
